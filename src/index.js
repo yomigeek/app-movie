12ff3da729ae786c663054da0ba778ec84c14a5b
yomigeek <seayomi@gmail.com>
2021-03-25 00:16:58 +0100

    implement design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import "./assets/styles/index.css";
 import App from "./components/App";
 
